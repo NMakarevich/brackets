bfef48cdda33581c6170cc65f265ad4aa297ade7
Nikolay Makarevich <49314579+NMakarevich@users.noreply.github.com>
2021-07-05 13:24:24 +0300

    fix: upgrade solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
   let brConf = bracketsConfig.map(brackets => brackets.join(''));
   if (!str.length % 2) return false; 
-  for (let i = 0; i <= str.length / 2 + 1; i++) {
+  while(true) {
+    let noMatchCount = 0;
     brConf.forEach(brackets => {
-      if (str.includes(brackets)) {
-        str = str.replace(brackets, '');
+      if (!str.includes(brackets)) {
+        noMatchCount++;
       }
+      str = str.replace(brackets, '')
     })
+    if (noMatchCount == brConf.length) break;
   } 
   return !str;
 }
