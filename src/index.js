378305821f21ad8d1f770e16338e453f270012dd
Nikolay Makarevich <49314579+NMakarevich@users.noreply.github.com>
2021-07-05 13:08:36 +0300

    feat: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brConf = bracketsConfig.map(brackets => brackets.join(''));
+  if (!str.length % 2) return false; 
+  for (let i = 0; i <= str.length / 2 + 1; i++) {
+    brConf.forEach(brackets => {
+      if (str.includes(brackets)) {
+        str = str.replace(brackets, '');
+      }
+    })
+  } 
+  return !str;
 }
